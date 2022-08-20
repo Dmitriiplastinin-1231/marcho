fd4aa579353d864ddd3fc4db0f32dc217c339c81
Dmitriiplastinin-1231 <dimanplastinen@gmail.com>
2022-08-20 21:21:46 +0300

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,30 @@
 $(function() {
+	$('.select-styler').styler();
+
+	$('.shop-content__filter-btn').on('click', function () {
+		$('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+		$(this).addClass('shop-content__filter-btn--active');
+	});
+
+	$('.button-list').on('click', function () {
+		$('.product-item').addClass('product-item--list');
+	});
+
+	$('.button-grid').on('click', function () {
+		$('.product-item').removeClass('product-item--list');
+	});
+
+	$('.filter-price__input').ionRangeSlider({
+		type: "double",
+		prefix: "$",
+		onChange: function (data) {
+    		$('.filter-price__from').text(data.from);
+    		$('.filter-price__to').text(data.to);
+        }
+	});
+
+
+
 	$('.top-slider__inner').slick({
 		dots: true,
 		arrows: false,
@@ -53,6 +79,7 @@ $(function() {
 		updateClock();
 		const timeinterval = setInterval(updateClock, 1000);
 	}
+
 
 	const deadline = $('.promo__clock').attr('data-time');
 	initializeClock('promo__clock', deadline);
